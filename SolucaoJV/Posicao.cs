0ed810e8016f24e8b50315fd8ff69184c3d5e6e0
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-06-06 10:20:24 -0300

    Jogada Invalida quando digita qualquer coisa diferente dos campos do tabuleiro

--- SolucaoJV/Posicao.cs
+++ SolucaoJV/Posicao.cs
@@ -13,16 +13,48 @@ namespace SolucaoJV
         {
             Partida.EscreverEm("Sua vez: ", 8, 15);
             string jogada = Console.ReadLine();
-            char linha = jogada[0];
-            int coluna = int.Parse(jogada[1] + "");
+            
+            if (JogadaValida(jogada))
+            {
+                char linha = jogada[0];
+                int coluna = int.Parse(jogada[1] + "");
+                InserirJogada(linha, coluna);
+            }
+            else
+            {
+                JogadaInvalida();   
+                InserirJogada();
+            }
+        }
 
-            InserirJogada(linha, coluna);
+        public bool JogadaValida(string str)
+        {
+            if ((str[0] == 'a' || str[0] == 'b' || str[0] == 'c') &&
+                (str[1] == '1' || str[1] == '2' || str[1] == '3'))
+            {
+                return true;
+            }
+            else
+            {
+                
+                return false;
+            }
         }
         public void InserirJogada(char linha, int coluna)
         {
             Linha = linha - 'a';
             Coluna = coluna - 1;
         }
-
+        public void JogadaInvalida()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Partida.EscreverEm("Posicação Inválida", 20, 15);
+            Partida.EscreverEm("Qualquer tecla continua!", 20, 16);
+            Console.ResetColor();
+            Console.ReadKey();
+            Partida.EscreverEm("                             ", 17, 15);
+            Partida.EscreverEm("                             ", 17, 16);
+        }
     }
 }
+
