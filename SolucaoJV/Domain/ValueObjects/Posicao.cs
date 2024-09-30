6598579858d3e9418a564f45d4c35d52e0e9ddf9
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2024-09-30 17:27:28 -0300

    Reorganizando o código em suas pastas...

--- SolucaoJV/Domain/ValueObjects/Posicao.cs
+++ SolucaoJV/Domain/ValueObjects/Posicao.cs
@@ -11,30 +11,6 @@ namespace SolucaoJV.Domain.ValueObjects
         public int Linha { get; set; }
         public int Coluna { get; set; }
 
-        
-        public void LerJogada()
-        {
-            try
-            {
-                string jogada = Console.ReadLine();
-
-                if (JogadaValida(jogada))
-                {
-                    char linha = jogada[0];
-                    int coluna = int.Parse(jogada[1] + "");
-                    //RegistrarJogada(linha, coluna);
-                }
-                else
-                {
-                    JogadaInvalida();
-                }
-            }
-            catch
-            {
-                JogadaInvalida();
-            }
-        }
-
         public bool JogadaValida(string str)
         {
             if ((str[0] == 'a' || str[0] == 'b' || str[0] == 'c') &&
@@ -42,36 +18,6 @@ namespace SolucaoJV.Domain.ValueObjects
                 return true;
             else
                 return false;
-        }
-
-        public void JogadaInvalida()
-        {
-            int t = 3;
-
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Red;
-
-            for (int i = 0; i < 3; i++)
-            {
-                PosicaoCursorMsg($"Jogada Inválida...({t})");
-                Thread.Sleep(1000);
-                t--;
-            }
-
-            LimpaMsg();
-        }
-
-        private void LimpaMsg()
-        {
-            PosicaoCursorMsg("                           ");
-            PosicaoCursorMsg("");
-        }
-
-        private void PosicaoCursorMsg(string msg)
-        {
-            Console.SetCursorPosition(17, 15);
-            Console.Write(msg);
-            Thread.Sleep(1000);
         }
     }
 }
--- SolucaoJV/UI/Controllers/PartidaController.cs
+++ SolucaoJV/UI/Controllers/PartidaController.cs
@@ -1,25 +1,78 @@
 ﻿using System;
 using SolucaoJV.Application.Services;
-using SolucaoJV.Domain.Entities;
-using SolucaoJV.Domain.Services;
-using System.Collections.Generic;
-using System.Text;
+using SolucaoJV.Domain.ValueObjects;
+using SolucaoJV.UI;
+using System.Threading;
 
 namespace SolucaoJV.UI.Controllers
 {
     internal class PartidaController
     {
+        private readonly PartidaService _partidaService;
+        private readonly ConsoleUI _CON
         public int Linha { get; set; }
         public int Coluna { get; set; }
-        public void MudarJogador(TipoJogador jogadorAtual)
-        {
-            if (jogadorAtual == TipoJogador.X)
-                jogadorAtual = TipoJogador.O;
-        }
         public void RegistrarJogada(int linha, int coluna)
         {
             Linha = linha - 'a';
             Coluna = coluna - 1;
+        }
+
+        public void LerJogada()
+        {
+            try
+            {
+                string jogada = Console.ReadLine();
+
+                if (JogadaValida(jogada))
+                {
+                    char linha = jogada[0];
+                    int coluna = int.Parse(jogada[1] + "");
+                    //RegistrarJogada(linha, coluna);
+                }
+                else
+                {
+                    JogadaInvalida();
+                }
+            }
+            catch
+            {
+                JogadaInvalida();
+            }
+        }
+        public void JogadaInvalida()
+        {
+            int t = 3;
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            for (int i = 0; i < 3; i++)
+            {
+                PosicaoCursorMsg($"Jogada Inválida...({t})");
+                Thread.Sleep(1000);
+                t--;
+            }
+
+            LimpaMsg();
+        }
+
+        private void LimpaMsg()
+        {
+            PosicaoCursorMsg("                           ");
+            PosicaoCursorMsg("");
+        }
+
+        private void PosicaoCursorMsg(string msg)
+        {
+            Console.SetCursorPosition(17, 15);
+            Console.Write(msg);
+            Thread.Sleep(1000);
+        }
+        public void MudarJogador(TipoJogador jogadorAtual)
+        {
+            if (jogadorAtual == TipoJogador.X)
+                jogadorAtual = TipoJogador.O;
         }
     }
 }
