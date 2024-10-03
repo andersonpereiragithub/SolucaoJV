70c685c224fe308e04e7ff98e54626e60f0d66c1
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2024-10-02 22:13:51 -0300

    Dependências e Instanciação
    Aplicar Injeção de Dependências ao invés de instanciar.

--- SolucaoJV/Program.cs
+++ SolucaoJV/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SolucaoJV.Application.Services;
 using SolucaoJV.Domain.Services;
 using SolucaoJV.UI.Views;
 using SolucaoJV.UI.Controllers;
+using System;
 
 namespace SolucaoJV.V
 {
@@ -10,16 +11,31 @@ namespace SolucaoJV.V
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro();
-            ConfiguraTela configura = new ConfiguraTela();
-            
-            configura.ViewTela();
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<Tabuleiro>()
+                .AddSingleton<ConfiguraTela>()
+                .AddSingleton<PartidaDomainService>()
+                .AddSingleton<PartidaController>()
+                .AddSingleton<PartidaAppService>()
+                .BuildServiceProvider();
 
-            PartidaDomainService partidaDomainService = new PartidaDomainService();
+            var configuraTela = serviceProvider.GetService<ConfiguraTela>();
+            var partidaAppService = serviceProvider.GetService<PartidaAppService>();
+            var partidaController = serviceProvider.GetRequiredService<PartidaController>();
 
-            PartidaAppService partidaAppService = new PartidaAppService(tabuleiro, partidaDomainService);
-            
+            configuraTela.ViewTela();
             partidaAppService.IniciarPartida();
+
+            //Tabuleiro tabuleiro = new Tabuleiro();
+            //ConfiguraTela configura = new ConfiguraTela();
+            
+            //configura.ViewTela();
+
+            //PartidaDomainService partidaDomainService = new PartidaDomainService();
+
+            //PartidaAppService partidaAppService = new PartidaAppService(tabuleiro, partidaDomainService);
+            
+            //partidaAppService.IniciarPartida();
         }
     }
 }
