2d85d2368b12e59e2717ff8b69bb60a3ad204201
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2026-09-13 21:17:43 -0300

    Centraliza layout e padroniza mensagens do jogo

--- SolucaoJV/UI/Views/LayoutConsole.cs
+++ SolucaoJV/UI/Views/LayoutConsole.cs
@@ -5,5 +5,15 @@
         public const int PosicaoEntradaX = 17;
         public const int PosicaoEntradaY = 15;
         public const int QuantidadeCaracteresApagar = 25;
+
+        public const int PosicaoMensagemReinicioX = 3;
+        public const int PosicaoMensagemReinicioY = 15;
+        public const int PosicaoRespostaReinicioX = 34;
+
+        public const int PosicaoResultadoX = 14;
+        public const int PosicaoResultadoY = 14;
+
+        public const int PosicaoMensagemJogadaX = 17;
+        public const int PosicaoMensagemJogadaY = 16;
     }
 }
--- SolucaoJV/UI/Views/MensagemService.cs
+++ SolucaoJV/UI/Views/MensagemService.cs
@@ -7,60 +7,69 @@ namespace SolucaoJV.UI.Views
 {
     public class MensagemService : IMensagemService
     {
+        private const int TempoMensagemInvalida = 3;
+        private const int IntervaloMensagemInvalida = 1000;
+
         public void ExibirVencedor(TipoJogador vencedor)
         {
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = vencedor == TipoJogador.X ? ConsoleColor.Red : ConsoleColor.DarkGreen;
 
-            Console.SetCursorPosition(14, 14);
+            Console.SetCursorPosition(LayoutConsole.PosicaoResultadoX, LayoutConsole.PosicaoResultadoY);
             Console.WriteLine($"---> VENCEU!!!");
         }
         public void ExibirEmpate()
         {
-            Console.SetCursorPosition(14, 14);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(LayoutConsole.PosicaoResultadoX, LayoutConsole.PosicaoResultadoY);
             Console.WriteLine($"Houve EMPATE!!!");
         }
         public void ExibirMensagemReinicio()
         {
-            Console.SetCursorPosition(3, 15);
+            Console.SetCursorPosition(LayoutConsole.PosicaoMensagemReinicioX, LayoutConsole.PosicaoMensagemReinicioY);
             Console.WriteLine("Deseja reiniciar o jogo? (s/n): ");
-            Console.SetCursorPosition(34, 15);
+            Console.SetCursorPosition(LayoutConsole.PosicaoRespostaReinicioX, LayoutConsole.PosicaoMensagemReinicioY);
         }
         public void ExibirPosicaoOcupada(int linha, int coluna)
         {
             string posicao = $"{(char)('a' + linha)}{coluna + 1}";
+            
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
 
-            Console.SetCursorPosition(17, 16);
+            Console.SetCursorPosition(LayoutConsole.PosicaoMensagemJogadaX, LayoutConsole.PosicaoMensagemJogadaY);
             Console.Write($"[{posicao}] já ocupada!");
         }
         public void LimparMensagemJogada()
         {
-            int colunaAtual = Console.CursorLeft;
-            int linhaAtual = Console.CursorTop;
+            int posicaoAtualX = Console.CursorLeft;
+            int posicaoAtualY = Console.CursorTop;
 
             ConsoleColor corFundoAtual = Console.BackgroundColor;
             ConsoleColor corTextoAtual = Console.ForegroundColor;
 
-            Console.SetCursorPosition(13, 16);
+            Console.SetCursorPosition(17, 16);
 
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.Write(new string(' ', 30));
+            Console.Write(new string(' ', LayoutConsole.QuantidadeCaracteresApagar));
 
             Console.BackgroundColor = corFundoAtual;
             Console.ForegroundColor = corTextoAtual;
 
-            Console.SetCursorPosition(colunaAtual, linhaAtual);
+            Console.SetCursorPosition(posicaoAtualX, posicaoAtualY);
         }
         public void ExibirJogadaInvalida()
         {
-            int tempo = 3;
-            for (int i = 0; i < 3; i++)
+            int tempo = TempoMensagemInvalida;
+
+            for (int i = 0; i < TempoMensagemInvalida; i++)
             {
                 Console.SetCursorPosition(LayoutConsole.PosicaoEntradaX, LayoutConsole.PosicaoEntradaY);
                 Console.WriteLine($"Jogada Inválida...({tempo})");
-                Thread.Sleep(1000);
+                Thread.Sleep(IntervaloMensagemInvalida);
                 tempo--;
             }
             Console.SetCursorPosition(LayoutConsole.PosicaoEntradaX, LayoutConsole.PosicaoEntradaY);
